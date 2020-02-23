26455b9e7176898f6de8660d18af7eeb1d480052
Alexander Makarof <makarofalex@gmail.com>
2020-02-23 16:50:47 +0300

    added react json

--- src/components/fields/index.js
+++ src/components/fields/index.js
@@ -1,31 +1,21 @@
 import React from "react";
+import ReactJson from "react-json-view";
 
-export const customInput = ({ label, input, type }) => {
+export const customInput = ({ label, input, type, ...props }) => {
   return (
     <div>
       <label>{label}</label>
       <input {...input} type={type} />
+      <ReactJson src={props} />
     </div>
   );
 };
 
-// export const customSelect = ({ label, input, type, ...props }) => {
-//   return (
-//     <div>
-//       <label>{label}</label>
-//       <select {...input} {...props} type={type}>
-//         <option value="tabs">Tabs</option>
-//         <option value="spaces">Spaces</option>
-//       </select>
-//     </div>
-//   );
-// };
-
-export const customSelect = props => {
+export const customSelect = ({ label, input, type, ...props }) => {
   return (
     <div>
-      <label>{props.label}</label>
-      <select {...props.input}>
+      <label>{label}</label>
+      <select {...input} {...props} type={type}>
         <option value="tabs">Tabs</option>
         <option value="spaces">Spaces</option>
       </select>
